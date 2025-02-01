e761e0ab36bb55572ccbe5c2cd5c5482a1173504
Shohrukh-Abduvakhidov <vaxidov011@gmail.com>
2025-02-01 20:50:11 +0500

    changes added

--- admin/products/js-module/api.js
+++ admin/products/js-module/api.js
@@ -1,13 +1,20 @@
-import { getData } from "../../../requests/request.js";
+import { getData , deleteData } from "../../../requests/request.js";
 import { get } from "./dom.js";
-
-export async function fetchData() {
+import { updateControl } from "./dom.js";
+export async function fetchData(params = "") {
   try {
-    let products = await getData("/products");
+    let products = await getData(`/products${params}`);
     console.log(products);
     get(products);
+    updateControl(products.length)
   } catch (error) {
     console.error(error);
     get(error);
   }
+}
+
+export async function deleteProduct(params = "")
+{
+    await deleteData(`/products/${params}`)
+    fetchData()
 }
--- admin/products/js-module/dom.js
+++ admin/products/js-module/dom.js
@@ -1,5 +1,7 @@
 let tbody = document.querySelector(".tbody");
-
+import { fetchData } from "./api.js";
+import { BASIC_API } from "../../../config.js";
+import { deleteProduct } from "./api.js";
 export function get(products) {
   tbody.innerHTML = "";
   products.forEach((product) => {
@@ -26,8 +28,44 @@ export function get(products) {
                 </div>
               </td>
         `;
-        tbody.append(tr)
-        let deleteBut = tr.querySelector("#deleteBut")
-        deleteBut.onclick = () => deleteProduct(product.id)
+    tbody.append(tr);
+    let deleteBut = tr.querySelector("#deleteBut");
+    deleteBut.onclick = () => deleteProduct(product.id);
   });
+}
+
+let nextBut = document.querySelector(".nextBut");
+let prevBut = document.querySelector(".prevBut");
+let numbers = document.querySelector(".numbers");
+
+let currentPage = 1;
+let limit = 5;
+
+export async function updateControl(products) {
+  let totalPages = Math.ceil(products / limit)  
+  prevBut.disabled = currentPage <= 1;
+  nextBut.disabled = currentPage >= totalPages;
+  numbers.innerHTML = "";
+  for (let i = 1; i <= totalPages; i++) {
+    let pageBut = document.createElement("span");
+    pageBut.innerHTML = i;
+    pageBut.classList.add("number");
+    if (i == currentPage) {
+      pageBut.classList.remove("number");
+      pageBut.classList.add("active");
+    }
+    numbers.append(pageBut);
+  }
+  prevBut.onclick = () => {
+    if (currentPage > 1) {
+      currentPage--;
+      fetchData(`/products?_page=${currentPage}&_per_page=${limit}`);
+    }
+  };
+  nextBut.onclick = () => {
+    if (currentPage < totalPages) {
+      currentPage++;
+      fetchData(`/products?_page=${currentPage}&_per_page=${limit}`);
+    }
+  };
 }
