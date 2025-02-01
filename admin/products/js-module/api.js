2184b7149238db7d7856686488d142d91c5c36de
Shohrukh-Abduvakhidov <vaxidov011@gmail.com>
2025-02-01 14:59:04 +0500

    prod

---
+++ admin/products/js-module/api.js
@@ -0,0 +1,13 @@
+import { getData } from "../../../requests/request.js";
+import { get } from "./dom.js";
+
+export async function fetchData() {
+  try {
+    let products = await getData("/products");
+    console.log(products);
+    get(products);
+  } catch (error) {
+    console.error(error);
+    get(error);
+  }
+}
