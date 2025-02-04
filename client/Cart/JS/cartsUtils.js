1aaef93ee34dab8daacba14e491cc57dec7aed10
Shohrukh-Abduvakhidov <vaxidov011@gmail.com>
2025-02-04 15:56:45 +0500

    saved changes

--- client/Cart/JS/cartsUtils.js
+++ client/Cart/JS/cartsUtils.js
@@ -1,9 +1,9 @@
 export function updateCartCount() {
     let cartCount = document.querySelector(".cntCartAdd");
-    if (!cartCount) return; // Если элемента нет на странице, просто выходим
+    if (!cartCount) return;
 
     let cart = JSON.parse(localStorage.getItem("cartSend")) || [];
-    let totalItems = cart.reduce((sum, item) => sum + (item.quantity || 1), 0); // Считаем общее количество
+    let totalItems = cart.reduce((sum, item) => sum + (item.quantity || 1), 0); 
 
     if (totalItems > 0) {
         cartCount.style.display = "flex";
@@ -13,7 +13,7 @@ export function updateCartCount() {
     }
 }
 
-// Обновляем счетчик на всех вкладках
-window.addEventListener("storage", () => {
+
+window.addEventListener("storage", () => {  
     updateCartCount();
 });
